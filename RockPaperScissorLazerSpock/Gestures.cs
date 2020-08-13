4236a751ebb8580a2dc928bf176d449fd1143523
NCRadtke00 <raddnc@gmail.com>
2020-08-13 09:20:29 -0500

    added gesture class and 5 moves

---
+++ RockPaperScissorLazerSpock/Gestures.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissorLazerSpock
+{
+    class Gestures
+    {
+        public string rock;
+        public string paper;
+        public string scissors;
+        public string laszer;
+        public string spock;
+        public List<Gestures> moves;
+        public Gestures()
+        {
+
+        }
+    }
+}
+}
