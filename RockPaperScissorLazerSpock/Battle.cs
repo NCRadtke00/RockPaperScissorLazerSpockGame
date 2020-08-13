d74ef998df8f6e5287e5c6aa2efd9db5e46f8b77
NCRadtke00 <raddnc@gmail.com>
2020-08-13 09:18:38 -0500

    added to battle class

--- RockPaperScissorLazerSpock/Battle.cs
+++ RockPaperScissorLazerSpock/Battle.cs
@@ -10,7 +10,38 @@ namespace RockPaperScissorLazerSpock
     {
         public Battle()
         {
+            public Battle()
+            {
+                Console.WriteLine("Do we have one person playing or two?");
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("-------------- ( 1 ) ----------------");
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("-------------- ( 2 ) ----------------");
+                Console.WriteLine("-------------------------------------");
+                string playerResult = Console.ReadLine();
+                if (playerResult == "1")
+                {
+                    RunSinglePlayerMode()
+                }
+                else if (playerResult == "2")
+                {
+                    RunTwoPlayerMode();
+                }
 
+
+            }
+            public void RunSinglePlayerMode()
+            {
+
+            }
+            public void RunTwoPlayerMode()
+            {
+
+            }
+            public void CompareMoves()
+            {
+
+            }
         }
     }
 }
